6cdf5169d28088ab97c6b6a6df21351ed58fd158
bjenne <bjenne@smail.uni-koeln.de>
2020-11-12 23:46:39 +0100

    Update main.java

--- main.java
+++ main.java
@@ -2,10 +2,9 @@ public class myAwesomeClass {
 
   public static  main(String args) {
     int myVariable = 15;
-    for (int j = 1; j < 20; i++) {
+    for (int j = 1; j < 20; j++) {
       myVariable = myVariable * j;
-      System.out.print(j)
-      System.out.println(" ");
-      System.outprintln(myVariable);
+      System.out.prinln(j + " " + myVariable);
     }
+}
 }
