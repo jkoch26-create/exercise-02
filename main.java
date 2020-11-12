cccd3cd657f38f588ac89a6f7ba6aacaa075e7e8
Valeria Gavrilova <gavrivalery@gmail.com>
2020-11-12 14:58:51 +0100

    Overflow and syntax errors fixed

--- main.java
+++ main.java
@@ -1,11 +1,11 @@
-public class myAwesomeClass {
+public class MyAwesomeClass {
 
-  public static  main(String args) {
-    int myVariable = 15;
-    for (int j = 1; j < 20; i++) {
-      myVariable = myVariable * j;
-      System.out.print(j)
-      System.out.println(" ");
-      System.outprintln(myVariable);
-    }
+  public static void main(String[] args) {
+      long myVariable = 15;
+      for (int j = 1; j < 20; j++) {
+        myVariable = myVariable * j;
+        System.out.println(j + " " + myVariable);
+      }
+  }
 }
+
